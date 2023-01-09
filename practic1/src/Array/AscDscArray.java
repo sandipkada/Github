2f3b37b34418c854fac21cdf18794b6cc4704e07
sandipkada <119999003+sandipkada@users.noreply.github.com>
2023-01-09 12:55:44 +0530

    Update AscDscArray.java
    userstory updated thank you

--- practic1/src/Array/AscDscArray.java
+++ practic1/src/Array/AscDscArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class AscDscArray {
 public static void main(String[] args) {
-	int s[]= {10,70,60,30,20,98 };
+	int s[]= {15,30,45,60,75,90 };
 	
 	for(int i=0;i<=s.length-1;i++) {
 		System.out.println(s[i]);
